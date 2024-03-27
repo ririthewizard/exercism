45c4125b2a59d22a2969e0c668a90d89194357ba
Riley <ririwilliams.rw@gmail.com>
2024-03-26 19:44:41 -0500

    Passed all tests

--- src/main/java/BirdWatcher.java
+++ src/main/java/BirdWatcher.java
@@ -1,32 +1,49 @@
 
 class BirdWatcher {
-    private final int[] birdsPerDay;
+    private int[] birdsPerDay = {2, 5, 0, 7, 4, 1};
 
     public BirdWatcher(int[] birdsPerDay) {
         this.birdsPerDay = birdsPerDay.clone();
     }
 
     public int[] getLastWeek() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.getLastWeek() method");
+        int[] lastWeeksBirds = {0, 2, 5, 3, 7, 8, 4};
+        return lastWeeksBirds;
     }
 
     public int getToday() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.getToday() method");
+        return birdsPerDay[birdsPerDay.length - 1];
     }
 
     public void incrementTodaysCount() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.incrementTodaysCount() method");
+        birdsPerDay[birdsPerDay.length - 1] = getToday() + 1;
     }
 
     public boolean hasDayWithoutBirds() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.hasDayWithoutBirds() method");
+        for (int day: birdsPerDay) {
+            return day == 0;
+        }
+        return false;
     }
 
     public int getCountForFirstDays(int numberOfDays) {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.getCountForFirstDays() method");
+        int visitingBirds = 0;
+
+        if (numberOfDays > birdsPerDay.length) {
+            numberOfDays = birdsPerDay.length;
+        }
+
+        for (int i = 0; i < numberOfDays; i++){
+            visitingBirds += birdsPerDay[i];
+        }
+        return visitingBirds;
     }
 
     public int getBusyDays() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.getBusyDays() method");
+        int busyDays = 0;
+        for (int day: birdsPerDay){
+            if (day >= 5){ busyDays++; }
+        }
+        return busyDays;
     }
 }
